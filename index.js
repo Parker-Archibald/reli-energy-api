bc8a3bdfd59c343f86dd1ed4ea3e407007b5c879
Parker <parkerarchibald97@gmail.com>
2022-10-14 07:52:34 -0600

    db

--- index.js
+++ index.js
@@ -7,11 +7,7 @@ let passwordHash = require('password-hash');
 app.use(express.json());
 app.use(cors());
 const DBConnect = {
-    host: '209.59.180.100',
-    port: '3306',
-    database: 'relie',
-    user: 'relie_user',
-    password: 'mqOI*YYt*)CW'
+    
 }
 
 app.get('/', (req, res) => {
